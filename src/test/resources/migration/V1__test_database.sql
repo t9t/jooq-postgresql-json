3996007a6969cb356513c2410ea9b217edf60e75
t9t <t9t@users.noreply.github.com>
2019-07-13 12:05:41 +0200

    Insert test data

--- src/test/resources/migration/V1__test_database.sql
+++ src/test/resources/migration/V1__test_database.sql
@@ -1,5 +1,12 @@
-create table jooq_jsonb_test
+create table jooq_json_test
 (
-    id   bigserial primary key,
-    data jsonb null
-)
+    id    bigserial primary key,
+    name  text  not null,
+    data  json  null,
+    datab jsonb null
+);
+
+insert into jooq_json_test (name, data, datab)
+values ('both', '{"json": {"int": 100, "str": "Hello, JSON world!", "object": {"v":  200}, "n": null}}', '{"jsonb": {"int": 100, "str": "Hello, JSON world!", "object": {"v": 200}, "n": null}}'),
+       ('empty', '{}', '{}'),
+       ('null', null, null);
