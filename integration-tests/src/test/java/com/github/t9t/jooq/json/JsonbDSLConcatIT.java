0b204c46d25c18a1e64fe487e733ba30141b3145
t9t <t9t@users.noreply.github.com>
2019-08-11 14:33:01 +0200

    Add tests for JsonbDSL.delete

--- integration-tests/src/test/java/com/github/t9t/jooq/json/JsonbDSLConcatIT.java
+++ integration-tests/src/test/java/com/github/t9t/jooq/json/JsonbDSLConcatIT.java
@@ -5,7 +5,6 @@ import org.junit.runners.Parameterized;
 import java.util.Arrays;
 import java.util.List;
 
-import static com.github.t9t.jooq.json.Jsonb.of;
 import static com.github.t9t.jooq.json.JsonbDSL.concat;
 import static com.github.t9t.jooq.json.JsonbDSL.field;
 
@@ -13,9 +12,10 @@ public class JsonbDSLConcatIT extends AbstractJsonDSLTest {
     @Parameterized.Parameters(name = "{0}")
     public static List<Object[]> params() {
         return generateParams("concat", Arrays.asList(
-                btest("objects").selecting(concat(field(of("{\"a\": 10}")), field(of("{\"b\": 20}")))).expectJsonb("{\"a\": 10, \"b\": 20}"),
-                btest("arrays").selecting(concat(field(of("[\"a\", 10, true]")), field(of("[50, {\"b\": 20}]")))).expectJsonb("[\"a\", 10, true, 50, {\"b\": 20}]"),
-                btest("arrayAndObject").selecting(concat(field(of("[\"a\", 10, true]")), field(of("{\"b\": 20}")))).expectJsonb("[\"a\", 10, true, {\"b\": 20}]")
+                btest("objects").selecting(concat(field("{\"a\": 10}"), field("{\"b\": 20}"))).expectJsonb("{\"a\": 10, \"b\": 20}"),
+                btest("arrays").selecting(concat(field("[\"a\", 10, true]"), field("[50, {\"b\": 20}]"))).expectJsonb("[\"a\", 10, true, 50, {\"b\": 20}]"),
+                btest("arrayAndObject").selecting(concat(field("[\"a\", 10, true]"), field("{\"b\": 20}"))).expectJsonb("[\"a\", 10, true, {\"b\": 20}]"),
+                btest("nested").selecting(concat(concat(field("{\"a\": 10}"), field("{\"b\": 20}")), field("{\"c\": 30}"))).expectJsonb("{\"a\": 10, \"b\": 20, \"c\": 30}")
         ));
     }
 }
---
+++ integration-tests/src/test/java/com/github/t9t/jooq/json/JsonbDSLDeleteIT.java
@@ -0,0 +1,23 @@
+package com.github.t9t.jooq.json;
+
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.github.t9t.jooq.json.JsonbDSL.delete;
+import static com.github.t9t.jooq.json.JsonbDSL.field;
+
+public class JsonbDSLDeleteIT extends AbstractJsonDSLTest {
+    @Parameterized.Parameters(name = "{0}")
+    public static List<Object[]> params() {
+        return generateParams("delete", Arrays.asList(
+                btest("obj").selecting(delete(field("{\"a\": 10, \"b\": 20}"), "a")).expectJsonb("{\"b\": 20}"),
+                btest("array").selecting(delete(field("[\"a\", 1, \"b\", false, \"c\"]"), "b")).expectJsonb("[\"a\", 1, false, \"c\"]"),
+                btest("nested").selecting(delete(delete(field("{\"a\": 10, \"b\": 20, \"c\": 30}"), "a"), "c")).expectJsonb("{\"b\": 20}"),
+
+                btest("multi_obj").selecting(delete(field("{\"a\": 10, \"b\": 20, \"c\": 30}"), "b", "a")).expectJsonb("{\"c\": 30}"),
+                btest("multi_array").selecting(delete(field("[\"a\", 1, \"b\", false, \"c\"]"), "b", "a")).expectJsonb("[1, false, \"c\"]")
+        ));
+    }
+}
