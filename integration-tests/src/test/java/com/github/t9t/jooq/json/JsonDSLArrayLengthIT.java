464558ff21069276150849df21c07d42f3221bbc
t9t <t9t@users.noreply.github.com>
2019-09-25 10:34:50 +0200

    Add json(b)_array_length

---
+++ integration-tests/src/test/java/com/github/t9t/jooq/json/JsonDSLArrayLengthIT.java
@@ -0,0 +1,21 @@
+package com.github.t9t.jooq.json;
+
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.github.t9t.jooq.json.JsonDSL.arrayLength;
+
+public class JsonDSLArrayLengthIT extends AbstractJsonDSLTest {
+    @Parameterized.Parameters(name = "{0}")
+    public static List<Object[]> params() {
+        return generateParams("arrayLength", Arrays.asList(
+                test("zeroLength").usingJson("[]").selecting(arrayLength(json)).expect(0),
+                test("nonZeroLength").usingJson("[1, \"b\", false]").selecting(arrayLength(json)).expect(3),
+
+                btest("zeroLength").usingJson("[]").selecting(JsonbDSL.arrayLength(jsonb)).expect(0),
+                btest("nonZeroLength").usingJson("[1, \"b\", false]").selecting(JsonbDSL.arrayLength(jsonb)).expect(3)
+        ));
+    }
+}
