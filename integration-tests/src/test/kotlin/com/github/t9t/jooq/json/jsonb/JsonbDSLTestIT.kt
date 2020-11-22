6e3b9ab4240ae1e002b55389f16b9a4e7e39a35e
davinkevin <davin.kevin@gmail.com>
2020-11-22 16:23:14 +0100

    feat(kotlin): provide operators through extension functions
    To use a more fluent syntax, we provide some extension functions in the kotlin context. All the code is just a bridge with real java implementation.
    
    Closes #11

---
+++ integration-tests/src/test/kotlin/com/github/t9t/jooq/json/jsonb/JsonbDSLTestIT.kt
@@ -0,0 +1,336 @@
+package com.github.t9t.jooq.json.jsonb
+
+import com.github.t9t.jooq.json.JsonbDSL
+import org.jooq.Field
+import org.jooq.JSONB
+import org.jooq.impl.DSL
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Created by kevin on 20/12/2020
+ */
+class JsonbDSLTestIT {
+
+    private val jsonbField: Field<JSONB> = DSL.field("foo.bar", JSONB::class.java)
+
+    @Test
+    fun `should provide extension function to create field from JSON`() {
+        /* Given */
+        val jsonb = JSONB.valueOf("{}")
+        /* When */
+        val jsonbField = JsonbDSL.field(jsonb)
+        val jsonbFieldExt = jsonb.toField()
+        /* Then */
+        assertEquals(jsonbField, jsonbFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for arrayElement`() {
+        /* Given */
+        val index = 1
+        /* When */
+        val arrayElementField = JsonbDSL.arrayElement(jsonbField, index)
+        val arrayElementFieldExt = jsonbField.arrayElement(index)
+        /* Then */
+        assertEquals(arrayElementField, arrayElementFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for arrayElementText`() {
+        /* Given */
+        val index = 1
+        /* When */
+        val arrayElementTextField = JsonbDSL.arrayElementText(jsonbField, index)
+        val arrayElementTextFieldExt = jsonbField.arrayElementText(index)
+        /* Then */
+        assertEquals(arrayElementTextField, arrayElementTextFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for fieldByKey`() {
+        /* Given */
+        val key = "key"
+        /* When */
+        val fieldByKeyField = JsonbDSL.fieldByKey(jsonbField, key)
+        val fieldByKeyFieldExt = jsonbField.fieldByKey(key)
+        /* Then */
+        assertEquals(fieldByKeyField, fieldByKeyFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for fieldByKeyText`() {
+        /* Given */
+        val key = "key"
+        /* When */
+        val fieldByKeyTextField = JsonbDSL.fieldByKeyText(jsonbField, key)
+        val fieldByKeyTextFieldExt = jsonbField.fieldByKeyText(key)
+        /* Then */
+        assertEquals(fieldByKeyTextField, fieldByKeyTextFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for objectAtPath with varargs`() {
+        /* Given */
+        val path = arrayOf("path", "to", "key")
+        /* When */
+        val objectAtPathField = JsonbDSL.objectAtPath(jsonbField, *path)
+        val objectAtPathFieldExt = jsonbField.objectAtPath(*path)
+        /* Then */
+        assertEquals(objectAtPathField, objectAtPathFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for objectAtPath with collection`() {
+        /* Given */
+        val path = arrayOf("path", "to", "key")
+        /* When */
+        val objectAtPathField = JsonbDSL.objectAtPath(jsonbField, path.toList())
+        val objectAtPathFieldExt = jsonbField.objectAtPath(path.toList())
+        /* Then */
+        assertEquals(objectAtPathField, objectAtPathFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for objectAtPathText with varargs`() {
+        /* Given */
+        val path = arrayOf("path", "to", "key")
+        /* When */
+        val objectAtPathTextField = JsonbDSL.objectAtPathText(jsonbField, *path)
+        val objectAtPathTextFieldExt = jsonbField.objectAtPathText(*path)
+        /* Then */
+        assertEquals(objectAtPathTextField, objectAtPathTextFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for objectAtPathText with collection`() {
+        /* Given */
+        val path = arrayOf("path", "to", "key")
+        /* When */
+        val objectAtPathTextField = JsonbDSL.objectAtPathText(jsonbField, path.toList())
+        val objectAtPathTextFieldExt = jsonbField.objectAtPathText(path.toList())
+        /* Then */
+        assertEquals(objectAtPathTextField, objectAtPathTextFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for contains`() {
+        /* Given */
+        val other: Field<JSONB> = DSL.field("other.bar", JSONB::class.java)
+        /* When */
+        val containsField = JsonbDSL.contains(jsonbField, other)
+        val containsFieldExt = jsonbField._contains(other)
+        /* Then */
+        assertEquals(containsField, containsFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for containedIn`() {
+        /* Given */
+        val other: Field<JSONB> = DSL.field("other.bar", JSONB::class.java)
+        /* When */
+        val containedInField = JsonbDSL.containedIn(jsonbField, other)
+        val containedInFieldExt = jsonbField.containedIn(other)
+        /* Then */
+        assertEquals(containedInField, containedInFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for hasKey`() {
+        /* Given */
+        val key = "bar"
+        /* When */
+        val hasKeyField = JsonbDSL.hasKey(jsonbField, key)
+        val hasKeyFieldExt = jsonbField.hasKey(key)
+        /* Then */
+        assertEquals(hasKeyField, hasKeyFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for hasAnyKey with varargs`() {
+        /* Given */
+        val keys = arrayOf("key1", "key2")
+        /* When */
+        val hasAnyKeyField = JsonbDSL.hasAnyKey(jsonbField, *keys)
+        val hasAnyKeyFieldExt = jsonbField.hasAnyKey(*keys)
+        /* Then */
+        assertEquals(hasAnyKeyField, hasAnyKeyFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for hasAnyKey with Collection`() {
+        /* Given */
+        val keys = arrayOf("key1", "key2")
+        /* When */
+        val hasAnyKeyField = JsonbDSL.hasAnyKey(jsonbField, keys.toList())
+        val hasAnyKeyFieldExt = jsonbField.hasAnyKey(keys.toList())
+        /* Then */
+        assertEquals(hasAnyKeyField, hasAnyKeyFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for hasAllKeys with varargs`() {
+        /* Given */
+        val keys = arrayOf("key1", "key2")
+        /* When */
+        val hasAllKeysField = JsonbDSL.hasAllKeys(jsonbField, *keys)
+        val hasAllKeysFieldExt = jsonbField.hasAllKeys(*keys)
+        /* Then */
+        assertEquals(hasAllKeysField, hasAllKeysFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for hasAllKeys with Collection`() {
+        /* Given */
+        val keys = arrayOf("key1", "key2")
+        /* When */
+        val hasAllKeysField = JsonbDSL.hasAllKeys(jsonbField, keys.toList())
+        val hasAllKeysFieldExt = jsonbField.hasAllKeys(keys.toList())
+        /* Then */
+        assertEquals(hasAllKeysField, hasAllKeysFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for concat`() {
+        /* Given */
+        val other: Field<JSONB> = DSL.field("other.bar", JSONB::class.java)
+        /* When */
+        val concatField = JsonbDSL.concat(jsonbField, other)
+        val concatFieldExt = jsonbField._concat(other)
+        /* Then */
+        assertEquals(concatField, concatFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for delete with key or element`() {
+        /* Given */
+        val key = "foo"
+        /* When */
+        val deleteField = JsonbDSL.delete(jsonbField, key)
+        val deleteFieldExt = jsonbField.delete(key)
+        /* Then */
+        assertEquals(deleteField, deleteFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for delete with varargs`() {
+        /* Given */
+        val keys = arrayOf("key1", "key2")
+        /* When */
+        val deleteField = JsonbDSL.delete(jsonbField, *keys)
+        val deleteFieldExt = jsonbField.delete(*keys)
+        /* Then */
+        assertEquals(deleteField, deleteFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for deleteElement`() {
+        /* Given */
+        val index = 1
+        /* When */
+        val deleteElementField = JsonbDSL.deleteElement(jsonbField, index)
+        val deleteElementFieldExt = jsonbField.deleteElement(index)
+        /* Then */
+        assertEquals(deleteElementField, deleteElementFieldExt)
+    }
+
+    @Test
+    fun `should provide extension function for deletePath`() {
+        /* Given */
+        val keys = arrayOf("key1", "key2")
+        /* When */
+        val deletePathField = JsonbDSL.deletePath(jsonbField, *keys)
+        val deletePathFieldExt = jsonbField.deletePath(*keys)
+        /* Then */
+        assertEquals(deletePathField, deletePathFieldExt)
+    }
+
+
+
+
+
+    @Test
+    fun `should provide function for arrayLength`() {
+        /* Given */
+        /* When */
+        val arrayLengthField = JsonbDSL.arrayLength(jsonbField)
+        val arrayLengthFieldExt = arrayLength(jsonbField)
+        /* Then */
+        assertEquals(arrayLengthField, arrayLengthFieldExt)
+    }
+
+    @Test
+    fun `should provide function for extractPath with varargs`() {
+        /* Given */
+        val path = arrayOf("path", "to", "key")
+        /* When */
+        val extractPathField = JsonbDSL.extractPath(jsonbField, *path)
+        val extractPathFieldExt = extractPath(jsonbField, *path)
+        /* Then */
+        assertEquals(extractPathField, extractPathFieldExt)
+    }
+
+    @Test
+    fun `should provide function for extractPath with collection`() {
+        /* Given */
+        val path = arrayOf("path", "to", "key")
+        /* When */
+        val extractPathField = JsonbDSL.extractPath(jsonbField, path.toList())
+        val extractPathFieldExt = extractPath(jsonbField, path.toList())
+        /* Then */
+        assertEquals(extractPathField, extractPathFieldExt)
+    }
+
+    @Test
+    fun `should provide function for extractPathText with varargs`() {
+        /* Given */
+        val path = arrayOf("path", "to", "key")
+        /* When */
+        val extractPathTextField = JsonbDSL.extractPathText(jsonbField, *path)
+        val extractPathTextFieldExt = extractPathText(jsonbField, *path)
+        /* Then */
+        assertEquals(extractPathTextField, extractPathTextFieldExt)
+    }
+
+    @Test
+    fun `should provide function for extractPathText with collection`() {
+        /* Given */
+        val path = arrayOf("path", "to", "key")
+        /* When */
+        val extractPathTextField = JsonbDSL.extractPathText(jsonbField, path.toList())
+        val extractPathTextFieldExt = extractPathText(jsonbField, path.toList())
+        /* Then */
+        assertEquals(extractPathTextField, extractPathTextFieldExt)
+    }
+
+    @Test
+    fun `should provide function for typeOf`() {
+        /* Given */
+        /* When */
+        val typeOfField = JsonbDSL.typeOf(jsonbField)
+        val typeOfFieldExt = typeOf(jsonbField)
+        /* Then */
+        assertEquals(typeOfField, typeOfFieldExt)
+    }
+
+    @Test
+    fun `should provide function for stripNulls`() {
+        /* Given */
+        /* When */
+        val stripNullsField = JsonbDSL.stripNulls(jsonbField)
+        val stripNullsFieldExt = stripNulls(jsonbField)
+        /* Then */
+        assertEquals(stripNullsField, stripNullsFieldExt)
+    }
+
+    @Test
+    fun `should provide function for pretty`() {
+        /* Given */
+        /* When */
+        val prettyField = JsonbDSL.pretty(jsonbField)
+        val prettyFieldExt = pretty(jsonbField)
+        /* Then */
+        assertEquals(prettyField, prettyFieldExt)
+    }
+
+}
---
+++ jooq-postgresql-json/src/main/kotlin/com/github/t9t/jooq/json/jsonb/JsonbDSL.kt
@@ -0,0 +1,151 @@
+package com.github.t9t.jooq.json.jsonb
+
+import com.github.t9t.jooq.json.JsonbDSL
+import org.jooq.Condition
+import org.jooq.Field
+import org.jooq.JSONB
+
+/**
+ * @see JsonbDSL.field
+ */
+fun JSONB.toField(): Field<JSONB> = JsonbDSL.field(this)
+
+/**
+ * @see JsonbDSL.arrayElement
+ */
+fun Field<JSONB>.arrayElement(index: Int): Field<JSONB> = JsonbDSL.arrayElement(this, index)
+
+/**
+ * @see JsonbDSL.arrayElementText
+ */
+fun Field<JSONB>.arrayElementText(index: Int): Field<String> = JsonbDSL.arrayElementText(this, index)
+
+/**
+ * @see JsonbDSL.fieldByKey
+ */
+fun Field<JSONB>.fieldByKey(key: String): Field<JSONB> = JsonbDSL.fieldByKey(this, key)
+
+/**
+ * @see JsonbDSL.fieldByKeyText
+ */
+fun Field<JSONB>.fieldByKeyText(key: String): Field<String> = JsonbDSL.fieldByKeyText(this, key)
+
+/**
+ * @see JsonbDSL.objectAtPath
+ */
+fun Field<JSONB>.objectAtPath(vararg path: String): Field<JSONB> = JsonbDSL.objectAtPath(this, *path)
+
+/**
+ * @see JsonbDSL.objectAtPath
+ */
+fun Field<JSONB>.objectAtPath(path: Collection<String>): Field<JSONB> = JsonbDSL.objectAtPath(this, path)
+
+/**
+ * @see JsonbDSL.objectAtPathText
+ */
+fun Field<JSONB>.objectAtPathText(vararg path: String): Field<String> = JsonbDSL.objectAtPathText(this, *path)
+
+/**
+ * @see JsonbDSL.objectAtPathText
+ */
+fun Field<JSONB>.objectAtPathText(path: Collection<String>): Field<String> = JsonbDSL.objectAtPathText(this, path)
+
+/**
+ * @see JsonbDSL.contains
+ */
+fun Field<JSONB>._contains(other: Field<JSONB>): Condition = JsonbDSL.contains(this, other)
+
+/**
+ * @see JsonbDSL.containedIn
+ */
+fun Field<JSONB>.containedIn(other: Field<JSONB>): Condition = JsonbDSL.containedIn(this, other)
+
+/**
+ * @see JsonbDSL.hasKey
+ */
+fun Field<JSONB>.hasKey(key: String): Condition = JsonbDSL.hasKey(this, key)
+
+/**
+ * @see JsonbDSL.hasAnyKey
+ */
+fun Field<JSONB>.hasAnyKey(vararg keys: String): Condition = JsonbDSL.hasAnyKey(this, *keys)
+
+/**
+ * @see JsonbDSL.hasAnyKey
+ */
+fun Field<JSONB>.hasAnyKey(keys: Collection<String>): Condition = JsonbDSL.hasAnyKey(this, keys)
+
+/**
+ * @see JsonbDSL.hasAllKeys
+ */
+fun Field<JSONB>.hasAllKeys(vararg keys: String): Condition = JsonbDSL.hasAllKeys(this, *keys)
+
+/**
+ * @see JsonbDSL.hasAllKeys
+ */
+fun Field<JSONB>.hasAllKeys(keys: Collection<String>): Condition = JsonbDSL.hasAllKeys(this, keys)
+
+/**
+ * @see JsonbDSL.concat
+ */
+fun Field<JSONB>._concat(field2: Field<JSONB>): Field<JSONB> = JsonbDSL.concat(this, field2)
+
+/**
+ * @see JsonbDSL.delete
+ */
+fun Field<JSONB>.delete(keyOrElement: String): Field<JSONB> = JsonbDSL.delete(this, keyOrElement)
+
+/**
+ * @see JsonbDSL.delete
+ */
+fun Field<JSONB>.delete(vararg keysOrElements: String): Field<JSONB> = JsonbDSL.delete(this, *keysOrElements)
+
+/**
+ * @see JsonbDSL.deleteElement
+ */
+fun Field<JSONB>.deleteElement(index: Int): Field<JSONB> = JsonbDSL.deleteElement(this, index)
+
+/**
+ * @see JsonbDSL.deletePath
+ */
+fun Field<JSONB>.deletePath(vararg path: String): Field<JSONB> = JsonbDSL.deletePath(this, *path)
+
+/**
+ * @see JsonbDSL.arrayLength
+ */
+fun arrayLength(jsonField: Field<JSONB>): Field<Int> = JsonbDSL.arrayLength(jsonField)
+
+/**
+ * @see JsonbDSL.extractPath
+ */
+fun extractPath(jsonField: Field<JSONB>, vararg path: String): Field<JSONB> = JsonbDSL.extractPath(jsonField, *path)
+
+/**
+ * @see JsonbDSL.extractPath
+ */
+fun extractPath(jsonField: Field<JSONB>, path: Collection<String>): Field<JSONB>  = JsonbDSL.extractPath(jsonField, *path.toTypedArray())
+
+/**
+ * @see JsonbDSL.extractPathText
+ */
+fun extractPathText(jsonField: Field<JSONB>, vararg path: String): Field<String> = JsonbDSL.extractPathText(jsonField, *path)
+
+/**
+ * @see JsonbDSL.extractPathText
+ */
+fun extractPathText(jsonField: Field<JSONB>, path: Collection<String>): Field<String> = JsonbDSL.extractPathText(jsonField, *path.toTypedArray())
+
+/**
+ * @see JsonbDSL.typeOf
+ */
+fun typeOf(jsonField: Field<JSONB>): Field<String> = JsonbDSL.typeOf(jsonField)
+
+/**
+ * @see JsonbDSL.stripNulls
+ */
+fun stripNulls(jsonField: Field<JSONB>): Field<JSONB> = JsonbDSL.stripNulls(jsonField)
+
+/**
+ * @see JsonbDSL.pretty
+ */
+fun pretty(jsonField: Field<JSONB>): Field<String> = JsonbDSL.pretty(jsonField)
