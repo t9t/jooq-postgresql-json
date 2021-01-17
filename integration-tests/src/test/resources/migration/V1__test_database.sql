84bdbd7650f98250b98d5328d86dc9e52a24ee26
t9t <t9t@users.noreply.github.com>
2021-01-17 10:29:38 +0100

    Add test table with non-null JSON fields, to see what code is generated by jOOQ

--- integration-tests/src/test/resources/migration/V1__test_database.sql
+++ integration-tests/src/test/resources/migration/V1__test_database.sql
@@ -13,3 +13,12 @@ create table jooq.json_str_test
     data  json        null,
     datab jsonb       null
 );
+
+-- To see what the jOOQ Kotlin code generator does with not-nullable JSON fields
+create table jooq.json_not_null
+(
+    id    bigserial primary key,
+    name  text unique not null,
+    data  json        not null,
+    datab jsonb       not null
+);
