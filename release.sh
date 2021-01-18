24c92910687557a4bf9c3309aef32c9e3344c974
t9t <t9t@users.noreply.github.com>
2021-01-18 20:38:57 +0100

    Enforce JDK8 in release.sh

--- release.sh
+++ release.sh
@@ -7,6 +7,14 @@ if [[ $# -lt 1 ]]; then
   exit 1
 fi
 
+JAVAC_VERSION=$(javac -version |& cut -d ' ' -f 2)
+echo "javac version: ${JAVAC_VERSION}"
+
+if [[ ${JAVAC_VERSION} != 1.8* ]]; then
+  echo 'Please use JDK8'
+  exit 1
+fi
+
 mvn -Prelease release:clean release:prepare -DpushChanges=false -Dresume=false -DreleaseVersion=${1}
 mvn -Prelease release:perform
 mvn -Prelease deploy
