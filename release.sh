f149285bebc4e417082e18fcd31f2ac58d7fae9e
t9t <t9t@users.noreply.github.com>
2021-07-28 10:32:36 +0200

    Full build in release.sh before attempting release

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euv
+set -eu
 
 if [[ $# -lt 1 ]]; then
   echo 'Usage: ./release.sh <new version>'
@@ -11,10 +11,11 @@ JAVAC_VERSION=$(javac -version |& cut -d ' ' -f 2)
 echo "javac version: ${JAVAC_VERSION}"
 
 if [[ ${JAVAC_VERSION} != 11* ]]; then
-  echo 'Please use JDK8'
+  echo 'Please use JDK 11'
   exit 1
 fi
 
+mvn clean verify
 mvn -Prelease release:clean release:prepare -DpushChanges=false -Dresume=false -DreleaseVersion=${1}
 mvn -Prelease release:perform
 mvn -Prelease deploy
