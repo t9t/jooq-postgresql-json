ff9506ee6e7e68783fe2aa1d7a841ef8617cff41
t9t <t9t@users.noreply.github.com>
2022-01-01 14:20:43 +0100

    More verbose output in release.sh

--- release.sh
+++ release.sh
@@ -15,7 +15,14 @@ if [[ ${JAVAC_VERSION} != 11* ]]; then
   exit 1
 fi
 
+echo '=== Clean verify ==='
 mvn clean verify
+
+echo '=== Release prepare ==='
 mvn -Prelease release:clean release:prepare -DpushChanges=false -Dresume=false -DreleaseVersion=${1}
+
+echo '=== Release perform ==='
 mvn -Prelease release:perform
+
+echo '=== Deploy ==='
 mvn -Prelease deploy
